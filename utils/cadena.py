e06100e75e301c13b39f2a30bd9beffdbdc3a968
bgf751 <baldemar.guzman@xpertcad.com>
2020-12-10 21:26:01 -0600

    FEAT: Se agrega la utileria de cadena

---
+++ utils/cadena.py
@@ -0,0 +1,10 @@
+
+# Funcion que sirve para saludar
+# Void
+def saludar(nombre):
+    print("Hola "+ nombre)
+
+#Funcion que sirve para concatenar nombres
+# Retorna un String 
+def concatenar_nombre(nombre, apellido):
+    return nombre + " " + apellido
